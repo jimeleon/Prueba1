aa0a6d2b6affcb6b446fb6976b4eb62ec864204f
jimleon <jleon30748@ufide.ac.cr>
2024-09-29 21:26:18 -0600

    PRUEBA

---
+++ src/main/java/com/mycompany/prueba/PRUEBA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prueba;
+
+/**
+ *
+ * @author HP
+ */
+public class PRUEBA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
